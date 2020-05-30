b9cce9bf5eb7d87596523ae76c92d53d40faa92b
EduardoRSSR <eduardorodriguesiff@gmail.com>
2020-05-30 09:59:24 -0300

    Correcting function
    Correcting function to call the Promise.

--- Exercícios/JS Assíncrono/4. Desafios/ex001/ex001.js
+++ Exercícios/JS Assíncrono/4. Desafios/ex001/ex001.js
@@ -1,12 +1,13 @@
+function chamaIdade(){
 
-var idade = window.document.getElementById('txtidade');
+var idade = window.document.getElementById('txtidade').value;
 
 function idadePromise(idade){
-    return new Promise(function(resolve,reject){
+    return new Promise(function(resolve , reject){
         setTimeout(function(){
-            if(idade.value >= 18){
-                resolve('Maior de idade');
-            }else if(idade.value <18){
+            if(idade >= 18){
+                resolve(`Maior de idade`);
+            }else if(idade < 18){
                 reject('Menor de idade')
             }
         },2000);
@@ -14,17 +15,16 @@ function idadePromise(idade){
 
 }
 
-idadePromise(idade)
+    idadePromise(idade)
 
-.then(function(response){
-    window.alert(response);
-})
+    .then(function(response){
+        window.alert(response);
+    })
+    
+    .catch(function(error){
+        window.alert(error);
+    
+    });
+            
 
-.catch(function(error){
-    window.alert(error);
-
-});
-
-
-
-
+}
