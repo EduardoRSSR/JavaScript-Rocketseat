728299a1ae9cf148f4c462a986618aeb6fb35a4d
EduardoRSSR <eduardorodriguesiff@gmail.com>
2020-06-11 23:28:45 -0300

    Starting webserver module

--- ES6+/static_class.js
+++
@@ -1,5 +0,0 @@
-class matematica{
-    static soma (n1, n2){
-        return n1+n2;
-    }
-}
---
+++ ES6+/webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    entry: './src/main.js',             //arquivo principal
+    output:{                         // pra qual lugar e arquivo vai enviar o codigo es6+
+        path: __dirname +'/public',                //local
+        filename: 'bundle.js',          //nome do arquivo
+    },
+    devServer: {
+        contentBase: __dirname + '/public'
+    },
+    module:{
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: 'babel-loader',
+                }
+            }      
+
+        ],
+    },
+}
