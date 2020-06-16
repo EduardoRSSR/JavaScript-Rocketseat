fd70609626e5622ed1c4e5feed2f30b64d443ea2
EduardoRSSR <eduardorodriguesiff@gmail.com>
2020-06-16 02:50:04 -0300

    Commiting app using ES6+
    Application to search for repositories on Github API and render on window.
    
    Ending the final module of ES6+ course, reforcing following concepts:
    
    - Classes
    - Arrow functions
    - Import/Export
    - Try/Catch + Async/Await
    - Desestructuring
    - Short syntax
    -

--- 2- ES6+/M3- Async_Awayt/src/main.js
+++ 2- ES6+/M3- Async_Awayt/src/main.js
@@ -1,37 +1,98 @@
-/*
-const minhaPromise = () => new Promise((resolve, reject) => {setTimeout(()=> {resolve ('ok')}, 2000)});
+import api from './api';
 
-//async function executaPromise(){
-  //  console.log(await minhaPromise());
-    //console.log(await minhaPromise());
-    //console.log(await minhaPromise());
-//}
-  
+class App{
+    constructor(){
+        this.repositories=[]; //salva os repositorios na string
 
+        this.formEl= document.getElementById('repo-form'); 
+        this.listEl= document.getElementById('repo-list'); 
+        this.inputEl=document.querySelector('input[name=repository]')
 
-const executaPromise = async () => {      //Transformando em arrow function
-    console.log(await minhaPromise());
-    console.log(await minhaPromise());
-    console.log(await minhaPromise());
+        this.registerHandlers();
 
+    }
 
-}
+    registerHandlers(){ //registra os eventos
+    this.formEl.onsubmit = event => this.addRepository(event); // quando submeter o formulário chama função pra add o repo 
+    }
 
-executaPromise();
+    setLoading(loading=true){
+        if(loading === true){
+            let loadingEl=document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando...'))
+            loadingEl.setAttribute('id','loading');
 
-
-import axios from 'axios';
-
-class Api {
-    static async getUserInfo(username) {
-        try{
-        const response = await axios.get(`https:/api.github.com/users/${username}`)
-        console.log(response);
-        }catch(err){
-            console.warn('Erro na API.')
+            this.formEl.appendChild(loadingEl);
+        }
+        else{
+            document.getElementById('loading').remove();
         }
     }
-}
-Api.getUserInfo('eduardorssr');
-*/
 
+
+    async addRepository(event){
+       event.preventDefault();
+
+       const repoInput = this.inputEl.value;
+
+       if(repoInput.length === 0)
+           return;
+       
+        this.setLoading();
+       try{
+       const response = await api.get(`/repos/${repoInput}`);
+       
+       const {name, description, html_url, owner:{avatar_url}} = response.data;
+       console.log(response);
+
+
+       this.repositories.push({
+            name,
+            description,
+            avatar_url,
+            html_url
+        });
+
+    this.inputEl.value=''
+    
+
+    this.render();
+    }catch(err){
+        console.log('erro')
+        window.alert('[ERROR] Repositório não encontrado.')
+    }
+    this.setLoading(false);
+}
+
+
+    render(){
+    this.listEl.innerHTML ='';
+    
+    this.repositories.forEach(repo=>{
+
+        let imgEl = document.createElement('img');
+        imgEl.setAttribute('src', repo.avatar_url);
+
+        let titleEl = document.createElement('strong');
+        titleEl.appendChild(document.createTextNode(repo.name));
+
+        let descriptionEl = document.createElement('p');
+        descriptionEl.appendChild(document.createTextNode(repo.description));
+
+        let linkEl = document.createElement('a');
+        linkEl.setAttribute('target','_blank');
+        linkEl.setAttribute('href', repo.html_url);
+        linkEl.appendChild(document.createTextNode(repo.html_url));
+
+        let listItemEl = document.createElement('li');
+
+        listItemEl.appendChild(imgEl);
+        listItemEl.appendChild(titleEl);
+        listItemEl.appendChild(descriptionEl);
+        listItemEl.appendChild(linkEl);
+
+        this.listEl.appendChild(listItemEl);
+    });
+}}
+
+new App();
